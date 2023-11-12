3b14eb02963d857ae7bdf71c04a7ea7273b9ac5b
Bruna Dias Pupo <brunapupopupo@gmail.com>
2023-11-12 15:26:51 -0300

    lista 2

---
+++ Lista2/002.py
@@ -0,0 +1,4 @@
+base = input("Informe o valor da base:")
+altura = input("Informe o valor da altura:")
+area_triangulo = (base * altura) / 2
+print(f"A área do triângulo é:{area_triangulo}")
---
+++ Lista2/005.py
@@ -0,0 +1,4 @@
+a = int(input("Informe o valor de a:"))
+x = int(input("Informe o valor de x:"))
+f = a * ( x  ** (a - 1))
+print(f"O resultado é:{f}")
