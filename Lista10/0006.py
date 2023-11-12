75829d69440b4993e7651f4b8f8cff6e6756b360
Bruna Pupo <112135250+Brunapupo@users.noreply.github.com>
2023-11-12 18:30:13 -0300

    Delete Lista10 directory

--- Lista10/0006.py
+++
@@ -1,9 +0,0 @@
-from math import factorial
-
-def listaFatorial(lista):
-   lista_fatorial = []
-   for numero in lista:
-       lista_fatorial.append(factorial(numero))
-   return lista_fatorial
-
-print(listaFatorial([3, 4, 5, 6]))
